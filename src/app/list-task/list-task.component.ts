c6ddbb7c2dbfb747c090d0e0997e34f66f45e4ec
Pranjal Saxena <pranjal.saxena@keysight.com>
2019-01-24 16:12:42 +0530

    Tried to implement delete by drag and drop

--- src/app/list-task/list-task.component.ts
+++ src/app/list-task/list-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../Task';
 import { TaskArrayService } from '../task-array.service';
 import { Router } from '@angular/router';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragRelease} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-list-task',
@@ -83,6 +84,22 @@ export class ListTaskComponent implements OnInit {
   dropped(){
     console.log("in dropped!");
     this.onDeleteAll();
+  }
+  dragReleased(event: CdkDragRelease) {
+    console.log(event);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    // if (event.previousContainer === event.container) {
+    //   moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    // } else {
+    //   transferArrayItem(event.previousContainer.data,
+    //                     event.container.data,
+    //                     event.previousIndex,
+    //                     event.currentIndex);
+                        
+    // }
   }
 
   dragStart(){
--- src/app/task-array.service.ts
+++ src/app/task-array.service.ts
@@ -9,7 +9,13 @@ export class TaskArrayService {
   tasks: Array<Task> = new Array() ;
   
   
-  constructor() { }
+  constructor() {
+      for(let i=1; i<=10 ; i++) {
+          let t = new Task(i.toString() , "Desc" , new Date() , new Date());
+          this.tasks.push(t);
+      }
+    
+  }
   pushATask(task: Task) {
     this.tasks.push(task);
   }
